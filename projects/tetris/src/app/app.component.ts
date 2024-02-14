047878bf17e1af4c66b425af5cbd57912e74daf5
Sebastián Rojas Ricaurte <sebastian.rojas@littio.co>
2024-02-14 01:37:59 -0500

    lib

--- projects/tetris/src/app/app.component.ts
+++ projects/tetris/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameEngineLibService } from '@game-engine-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tetris';
+  constructor(private engineService: GameEngineLibService) {
+    console.info(engineService.testing);
+  }
 }
